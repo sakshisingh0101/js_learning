38db09ace264221e93db71d4f158e1f763492962
sakshisingh0101 <165368380+sakshisingh0101@users.noreply.github.com>
2024-05-23 11:18:24 +0000

    02_datatypes

---
+++ basics/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //allow us to use the newer version or treat all js code ad newer verison 
+//alert(3 + 3) // we are not working on browser ,there is a diff syntax to use this in nodejs
+console.log(2
+    +9
+
+)//readability should be high, not like this
+console.log("blah blah blah")
+
+let name="Unnati"
+let age=19
+let bool=true 
+
+// datatypes
+// number
+//longint
+//string
+//boolean =>true/false
+//null => object
+//undefined =>undefined
+//object
+//symbol
+console.log(typeof(null))
+console.log(typeof(undefined))
+console.log(typeof(name))
+console.log(typeof(age))
