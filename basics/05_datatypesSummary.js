ba0cacfd750bafdfa1c4ae083e37f2d94d55cd7b
sakshisingh0101 <165368380+sakshisingh0101@users.noreply.github.com>
2024-05-23 12:49:03 +0000

    summary datatypes

---
+++ basics/05_datatypesSummary.js
@@ -0,0 +1,17 @@
+//two types of datatype 
+//basis : how data is stored and accessed
+/*Primitive datatype(call by value)
+  number,string,boolean,null,symbol,undefined,bigint
+*/
+/*Non Primitive : 
+arrays,functions,objects
+*/
+let arr=[1,2,3,4,5]
+let obj={
+    name:"Sakshi",
+    age:19
+}
+
+let myfun=function(){
+    console.log("hello")
+}
