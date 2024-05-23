5fdf13295422c8294d4f52c82ed10432e8dcf08a
sakshisingh0101 <165368380+sakshisingh0101@users.noreply.github.com>
2024-05-23 10:38:45 +0000

    01_variables

---
+++ basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =122345
+let accountEmail ="sakshiaingh0101@gmail.com"
+var accountPassword ="ssingh1234"
+let accountCity="Delhi"
+let accountState;
+
+//accountId = 12345 //not allowed
+accountEmail="gunnu@gmail.com"
+accountCity="Mumbai"
+accountPassword="87431"
+
+/*prefer not to use var bcoz of problem with scope and all
+*/
+
+console.log(accountId)
+console.table([accountId , accountEmail, accountPassword,accountCity,accountState])
