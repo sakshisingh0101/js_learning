59546b43fc368f9eab52bd94157aa5a3272ae144
sakshisingh0101 <165368380+sakshisingh0101@users.noreply.github.com>
2024-05-23 11:47:35 +0000

    conversion

---
+++ basics/03_conversionoperator.js
@@ -0,0 +1,16 @@
+let score=undefined
+console.log(typeof score)
+let inNumber=Number(score)
+console.log(typeof inNumber)
+console.log( inNumber)
+//null ->0
+//undefined-> NaN 
+//which can not be converted in number it shows NaN (not a number)
+//1 or any other string of number->true
+//empty string ->false
+//null->false
+//undefined ->false
+let n;
+let convertBool =Boolean(n);
+console.log(convertBool);
+
