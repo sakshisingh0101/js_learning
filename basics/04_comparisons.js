2cc700691522d4caa64c41ff5e457eef01a16a11
sakshisingh0101 <165368380+sakshisingh0101@users.noreply.github.com>
2024-05-23 12:27:24 +0000

    comparisons

---
+++ basics/04_comparisons.js
@@ -0,0 +1,12 @@
+//comparisons
+/* ==,>,<,<=,>=,!=*/
+console.log("1">2)//false
+console.log(2<"3");//true
+console.log(null==0);//false 
+console.log(null>=0);//true
+console.log(null<0);//false
+console.log(undefined==0);//false
+console.log(undefined>0);//false
+console.log(undefined<=0);//false
+//== ,<=and>= work differntely in js
+//strict check which also check datatype and data conversion do not occur here(===)
