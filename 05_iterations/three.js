a6c226f29008de38a71934787862f55f41d2fef0
sakshisingh0101 <165368380+sakshisingh0101@users.noreply.github.com>
2024-05-29 10:26:44 +0000

    changes

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -25,8 +25,8 @@ map_1.set("Gujarat","Gandhi Nagar")
 //     console.log(key);
 // }
 // console.log(map_1.get("Up"));
-map_1.delete("Up")
-console.log(map_1.size)
+// map_1.delete("Up")
+// console.log(map_1.size)
 // //destructor of key and value
 // for (const [key,value] of map_1) {
 //     console.log(key + "=>" + value);
@@ -47,47 +47,48 @@ let course={
 // for (const key in course) {
 //     console.log(course[key]);
 // }
-// let arr=[1,2,3]
-// for(const i in arr){
-//     console.log(arr[i]);
-// }
+let arr=[1,2,3]
+for(const i in arr){
+    console.log(arr[i]);
+}
+//for in loop is not for maps
 // for(const key in map_1){
 //     console.log(key);
 // }
 
 
 
-//for each loop
-let coding=['py','js','cpp','c','rb']
-// coding.forEach(function (index){
-// console.log(index);
-// })
-// coding.forEach((item)=>{
+// //for each loop
+// let coding=['py','js','cpp','c','rb']
+// // coding.forEach(function (index){
+// // console.log(index);
+// // })
+// // coding.forEach((item)=>{
+// //     console.log(item);
+// // })
+// function print(item)
+// {
 //     console.log(item);
+// }
+// coding.forEach(print)
+// coding.forEach((item,index,arr)=>{
+//     console.log(item , index ,arr);
 // })
-function print(item)
-{
-    console.log(item);
-}
-coding.forEach(print)
-coding.forEach((item,index,arr)=>{
-    console.log(item , index ,arr);
-})
-let arr=[
-    {
-        language:"java",
-        ext:"java"
-    },
-    {
-        language:"c++",
-        ext:"cpp"
-    },
-    {
-        language:"javascript",
-        ext:"js"
-    }
-]
-arr.forEach( (item)=>{
-    console.log(item.language + ":-" + item.ext);
+// // let arr=[
+// //     {
+// //         language:"java",
+// //         ext:"java"
+// //     },
+// //     {
+// //         language:"c++",
+// //         ext:"cpp"
+// //     },
+// //     {
+// //         language:"javascript",
+// //         ext:"js"
+// //     }
+// // ]
+// // arr.forEach( (item)=>{
+// //     console.log(item.language + ":-" + item.ext);
 
-})
+// // })
